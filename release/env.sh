7ef4124061eef3d2f6f78ebd994ff634998d9018
Uli Heller <uli.heller@daemons-point.com>
2015-10-08 07:11:23 +0200

    Fixed version detection

--- release/env.sh
+++ release/env.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
-export GITBUCKET_VERSION=`cat ../project/build.scala | grep 'val Version' | cut -d \" -f 2`
+for bs in ../project/build.scala "${D}/project/build.scala" project/build.scala; do
+  if [ -s "${bs}" ]; then  
+    BUILD_SCALA="${bs}"
+    break
+  fi
+done
+
+export GITBUCKET_VERSION=`cat "${BUILD_SCALA}" | grep 'val Version' | cut -d \" -f 2`
 echo "GITBUCKET_VERSION: $GITBUCKET_VERSION"
